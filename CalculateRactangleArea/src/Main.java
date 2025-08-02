f71af0250eb4197414da976c36dcdfe176b50319
okkessefa <oksekesfackn53@gmail.com>
2025-08-02 23:41:23 +0300

    Add Calculate Rectangle Area Java project

---
+++ CalculateRactangleArea/src/Main.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] arg){
+
+        double width = 0;
+        double height = 0;
+        double area = 0;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the width: ");
+        width = scanner.nextDouble();
+
+        System.out.print("Enter the height: ");
+        height = scanner.nextDouble();
+
+        area = width * height / 2;
+
+        System.out.println("The area of rectangle is " + area + " cm^2.");
+
+        scanner.close();
+
+    }
+}
